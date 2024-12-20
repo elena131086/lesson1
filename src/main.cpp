56c9b3bb30ded7fb5d19fe13b47e899ae0d18db9
elena131086 <e131086@icloud.com>
2024-12-20 21:23:18 +0300

    factorial

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int result = 1;
+    int key = 0;
+    
+    cin >> key;
+    
+    for(int i = 1; i <=key; i++){
+        result = result * i;
+    }
+    cout << result;
+    return 0;
+}
