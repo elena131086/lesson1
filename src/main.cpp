de40e436d569b62b2cbe8121c64610b14cb2edc8
elena131086 <e131086@icloud.com>
2024-12-27 21:17:43 +0300

    MenuArrays

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
+#define SIZE 3
 using namespace std;
 int main(){
-    int result = 1;
-    int key = 0;
     
-    cin >> key;
+     const string play_ [] = {"_play_","settings","exit"};
+    const string settings_ [] = {"play","_settings_","exit"};
+    const string exit_ [] = {"play","settings","_exit_"};
+     int current = 0;
+         while(true){
+        if (current == 0) 
+        {
+           for (int i = 0;i< size;i++)
+            cout << play_ <<[i] endl;
+          } else if (current == 1){
+            for ( int i = 0; i < size; i++)
+            
+            cout << settings_ <<[i] endl;
+            }  else if (current == 2){
+           for ( int i=0;i<size;i++)
+            cout << exit_ <<[i] endl;
+            }  
+            
+                    
+                }
+             
+       
+
     
-    for(int i = 1; i <=key; i++){
-        result = result * i;
-    }
-    cout << result;
+    
+   
+   
     return 0;
 }
