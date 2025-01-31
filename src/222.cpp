f0d9a5069494eda945252f5cb0c283df3405782e
elena131086 <e131086@icloud.com>
2025-01-31 21:19:14 +0300

    33

--- src/222.cpp
+++ src/222.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[3][5]={{1,2,3,4,5},
-                    {10,20,30,40,50},
-                     {100,200,300,400,500}}; 
-            cout << arr[0][0] <<endl;  
-           for (int i= 2;i >= 0;i--){
-            for(int j=4;j>=0;j--){
-                cout << char( arr[i][j] )<< " ";
-            }
-            cout << endl;
+    int arr[3][2][2]={{    {1,2},
+                           {34,4}
+                         },
+                         {  {32,43},
+                            {54,21} 
+                        },
+                        {   {23,76},
+                            {87,45}
+                        }
+                        };
+            cout << arr[0][0][1] <<endl;  
+           for (int i= 0;i < 3;i++){
+           
+
+                for(int j=1;j<2;j++){
+                    for(int k = 1;k<2;k++){
+                        cout <<  arr[i][j][k]<< " ";
+                    }
+                    cout << endl;
+                }
+                cout << endl;
             }
 }
